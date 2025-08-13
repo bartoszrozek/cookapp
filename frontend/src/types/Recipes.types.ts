3e81c5e60be9a290bea87fbe6c741689e9537e43
bartoszrozek <rozuu.8@gmail.com>
2025-08-13 19:14:03 +0200

    refact: Clean up types files

--- frontend/src/types/Recipes.types.ts
+++ frontend/src/types/Recipes.types.ts
@@ -28,15 +28,3 @@ export interface AddRecipeModalProps {
     onClose: () => void;
     onAdd: (recipe: Recipe) => void;
 }
-
-export type AddRecipeState = Omit<Recipe, "id" | "created_at" | "updated_at" | "recipe_ingredients"> & { ingredients: IngredientInRecipe[] };
-
-export type Action = { type: string; value?: any; idx?: number; field?: string };
-
-export type IngredientInputProps = {
-    idx: number;
-    ingredient: IngredientInRecipe;
-    allIngredients: Ingredient[];
-    ingredientsLength: number;
-    dispatch: React.Dispatch<Action>;
-};
